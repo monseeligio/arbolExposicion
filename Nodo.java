827e6b014a5837a1c635f2c1bd01b28984b525fc
monseeligio <71795808+monseeligio@users.noreply.github.com>
2020-12-06 21:34:10 -0600

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,13 @@
+package arboles;
+
+public class Nodo {
+   int elemento;
+    Nodo izquierdo;
+    Nodo derecho;
+    public  Nodo (int elemento){
+        this.elemento=elemento;
+        this.izquierdo=null; 
+        this.derecho=null;
+        
+} 
+}
